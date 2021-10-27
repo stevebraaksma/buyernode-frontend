b116f69b964d1417e70e435e9c73bb726c889cf9
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-27 13:57:09 -0500

    add form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,41 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 
 function Index(props) {
+    const [newForm, setNewForm] =useState({
+        salesOrder: "",
+        customer: "",
+        assemblyNumber: "",
+        assemblyQty: "",
+    });
+
+    // handleChange function for form
+    const handleChange = (event) => {
+        setNewForm((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    // handleSubmit function for form
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.createTask(newForm);
+        setNewForm({
+            salesOrder: "",
+            customer: "",
+            assemblyNumber: "",
+            assemblyQty: "",            
+        });
+    };
+
+
+
+
+
+
     const loaded = () => {
         return props.tasks.map((task) => (
             <div key={task._id} className="task">
@@ -17,7 +50,38 @@ function Index(props) {
         return <h1>Loading...</h1>
     }
 
-    return props.tasks ? loaded() : loading();
-}
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={newForm.salesOrder} 
+                    placeholder="Sales Order #" 
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    value={newForm.customer} 
+                    placeholder="Customer" 
+                    onChange={handleChange}
+                />                
+                <input 
+                    type="text" 
+                    value={newForm.assemblyNumber} 
+                    placeholder="Assembly #" 
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    value={newForm.assemblyQty} 
+                    placeholder="Assembly Qty" 
+                    onChange={handleChange}
+                />                
+
+            </form>
+            {props.tasks ? loaded() : loading()}
+        </section>
+    );
+};
 
 export default Index;
