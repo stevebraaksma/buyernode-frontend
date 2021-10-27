e3be0099388b98ee38efa2863ac81953ba470fbf
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-27 10:19:34 -0500

    add components and pages

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+
+function Main(props) {
+
+
+
+
+    return <h1>Main</h1>;
+
+
+
+
+
+
+
+}
+
+export default Main;
