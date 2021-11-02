52b33a4fdb35fc7883b0366dbe80a6d59b7980ae
Steve Braaksma <stevebraaksma@hotmail.com>
2021-11-01 21:09:02 -0500

    lower case file name for react-bootstrap dropdown

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,74 +1,22 @@
-import { useEffect, useState } from "react";
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-
-
-
-// const handleChange = (event) => {
-//     setNewForm((prevState) => ({
-//         ...prevState,
-//         [event.target.name]: event.target.value
-        
-//     }));
-// };
-
+import DropdownButton from 'react-bootstrap/dropdownbutton';
+import Dropdown from 'react-bootstrap/dropdown';
 
 const Button = (props) => {
-    
-    
-    // const [ value, setValue ]=useState(props.task.userWorking);
-    const [tasks, setTasks] = useState(null);
 
     const handleSelect=(event)=> {
-        // console.log(event);
-        // console.log(props.task.userWorking);
         props.task.userWorking = event;
-        // console.log(props.task.userWorking)
-        // console.log(props.task)
-        
-       
-        // ****this one may not be necessary or may be duplicated
-        // setValue(event);
-        // console.log(event)
         updateTasks();
-        
     }
     
-
     const URL = "https://buyernode.herokuapp.com/tasks";
 
-    const getTasks = async () => {
-        const response = await fetch(URL);
-        const data = await response.json();
-        setTasks(data);
-    }
 
-    const createTasks = async (task) => {
-        await fetch(URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json",
-            },
-            body: JSON.stringify(task),
-        });
-        // update task list
-        getTasks();
-    }
-
-// need to figure out how to get id to be correct
+// update userWorking as per selected dropdown
     const updateTasks = async (task, id) => {
-        console.log('bingo')
-        console.log(props.task.userWorking)
-        console.log(props)  // this has what I need!!!!!
-        console.log(props.task)
-        console.log(props.task._id)
         const selectedId = props.task._id
-
         const destructuredTask = {
             userWorking: props.task.userWorking
         }
-        console.log(destructuredTask);
         await fetch(URL + "/" + selectedId, {
             method: "PUT",
             headers: {
@@ -76,32 +24,9 @@ const Button = (props) => {
             },
             body: JSON.stringify(destructuredTask),
         });  
-         // commented out, we still work******
-        // update list of tasks
-        // getTasks();
-        // console.log('bingobingo')
     };
 
-    // const deleteTasks = async (id) => {
-    //     await fetch(URL + "/" + id, {
-    //         method: "DELETE",
-    //     });
-    //     // update list of tasks
-    //     getTasks();
-    // };
-
-    
-
-    // useEffect(() => {
-    //     console.log("mrstateus");
-    // }, [value]);
-
-
     return (
-        // <a href={props.url} className="btn btn-primary">Info</a>
-
-
-
         <div className="dropdown-buyer">
             <DropdownButton 
                 id="dropdown-basic-button" 
@@ -110,11 +35,10 @@ const Button = (props) => {
             >
                 <Dropdown.Item eventKey="Buyer #1">Buyer 1</Dropdown.Item>
                 <Dropdown.Item eventKey="Buyer #2">Buyer 2</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Buyer 3</Dropdown.Item>
+                <Dropdown.Item eventKey="Buyer #3">Buyer 3</Dropdown.Item>
             </DropdownButton>
-            {/* <h4>You selected {value}</h4> */}
+            <br />
         </div>
-
     )
 }
 
